da3c340c5d93ddf4e5953509e0fe7d1a24731b3d
Chernyi Nikita Vladimirovich <chernyj@ccfit.nsu.ru>
2017-12-23 08:49:22 +0700

    new file:   Lab_2/.testdot 	new file:   Lab_2/lets_share.sh 	new file:   Lab_2/permissions.sh 	new file:   Lab_2/shared_chernyj.txt 	deleted:    Lab_2/shared_miiiiine.txt

---
+++ Lab_2/lets_share.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+scp ~/Labs/Lab_2/shared_chernyj.txt maseevskii@mephisto:~/labs/lab2/
+scp ~/Labs/Lab_2/shared_chernyj maseevskii@mephisto:~/labs/lab2
---
+++ Lab_2/permissions.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chmod o-rwx shared_chernyj.txt
+chmod u-wx shared_chernyj.txt
